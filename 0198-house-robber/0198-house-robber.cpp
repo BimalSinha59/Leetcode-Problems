cbe173b7f8ac67101795b4e81bd71c17db74b0eb
BimalSinha59 <bimalsinha6264@gmail.com>
2025-03-13 00:06:40 +0530

    Time: 0 ms (100%), Space: 10.6 MB (62.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
+    int solve(int idx,vector<int>& nums,vector<int>& dp){
+        if(idx==0){
+            return nums[0];
+        }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        int take=nums[idx];
+        if(idx>1){
+            take+=solve(idx-2,nums,dp);
+        }
+        int dtake=0+solve(idx-1,nums,dp);
+        return dp[idx]=max(take,dtake);
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int prev=nums[0];
-        int prev2=0;
-        for(int i=1; i<n; i++){
-            int take=nums[i];
-            if(i>1){
-                take+=prev2;
-            }
-            int dtake=0+prev;
-            int curr=max(take,dtake);
-            prev2=prev;
-            prev=curr;
-        }
-        return prev;
+        vector<int>dp(n,-1);
+        return solve(n-1,nums,dp);
+
+        // int prev=nums[0];
+        // int prev2=0;
+        // for(int i=1; i<n; i++){
+        //     int take=nums[i];
+        //     if(i>1){
+        //         take+=prev2;
+        //     }
+        //     int dtake=0+prev;
+        //     int curr=max(take,dtake);
+        //     prev2=prev;
+        //     prev=curr;
+        // }
+        // return prev;
         
     }
 };
