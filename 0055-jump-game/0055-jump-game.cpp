56c0c77fcdc9cdcac126a372843b5d0b0d906928
BimalSinha59 <bimalsinha6264@gmail.com>
2025-03-21 17:41:01 +0530

    Time: 0 ms (100%), Space: 52.4 MB (31.7%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
-    bool f(vector<int>& nums,int idx,int n,vector<pair<int,bool>>& dp){
-        if(idx==n-1){
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+
+        int temp = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + nums[i] >= temp) {
+                temp = i;
+            }
+        }
+        cout<<temp;
+        if (temp == 0) {
             return true;
         }
-        if(idx>=n){
-            return false;
-        }
-        if(dp[idx].first!=-1){
-            return dp[idx].second;
-        }
-        int x=nums[idx];
-        bool temp=false;
-        for(int i=1; i<=x; i++){
-            
-            if(idx+i<n){
-                bool temp2=f(nums,idx+i,n,dp);
-                dp[idx+i].first=idx+i;
-                dp[idx+i].second=temp2;
-                temp=temp || temp2;
-            }
-            else{
-                break;
-            }
-        }
-        dp[idx].first=idx;
-        return dp[idx].second=temp;
-    }
-    bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        vector<pair<int,bool>>dp;
-        for(int i=0; i<n; i++){
-            dp.push_back({-1,false});
-        }
-        return f(nums,0,n,dp);
-        
+        return false;
     }
 };
