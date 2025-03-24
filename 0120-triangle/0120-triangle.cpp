5d80f5104ca2d07a8c0eaab5ab15db4e266b4654
BimalSinha59 <bimalsinha6264@gmail.com>
2025-03-24 13:22:32 +0530

    Time: 3 ms (14.32%), Space: 12.7 MB (72.84%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+     /**************Recursion with Memoization******************/
+
     // int f(int i,int j,vector<vector<int>>& triangle,int n,vector<vector<int>>& dp){
     //     if(i==n-1){
     //         return triangle[n-1][j];
@@ -13,20 +15,43 @@ public:
     // }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
+        //vector<vector<int>>dp(n,vector<int>(n,0));
         //return f(0,0,triangle,n,dp);
 
+
+
+        /****************Tabulation***************/
+        
+        // for(int i=0; i<n; i++){
+        //     dp[n-1][i]=triangle[n-1][i];
+        // }
+
+        // for(int i=n-2; i>=0; i--){
+        //     for(int j=i; j>=0; j--){
+        //         int down=triangle[i][j]+dp[i+1][j];
+        //         int drdgnl=triangle[i][j]+dp[i+1][j+1];
+        //         dp[i][j]=min(down,drdgnl);
+        //     }
+        // }
+        // return dp[0][0];
+
+
+
+
+        /**************Space Optimization********************/
+        vector<int>front(n,0),curr(n,0);
         for(int i=0; i<n; i++){
-            dp[n-1][i]=triangle[n-1][i];
+            front[i]=triangle[n-1][i];
         }
 
         for(int i=n-2; i>=0; i--){
             for(int j=i; j>=0; j--){
-                int down=triangle[i][j]+dp[i+1][j];
-                int drdgnl=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(down,drdgnl);
+                int down=triangle[i][j]+front[j];
+                int drdgnl=triangle[i][j]+front[j+1];
+                curr[j]=min(down,drdgnl);
             }
+            front=curr;
         }
-        return dp[0][0];
+        return front[0];
     }
 };
