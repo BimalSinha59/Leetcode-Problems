ba89ea63d8523c4e8f25d6b60535fbe96fd7c562
BimalSinha59 <bimalsinha6264@gmail.com>
2025-04-02 18:48:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int f(int idx,int val,vector<int>& coins,vector<vector<int>>& dp){
+        if(idx==0){
+            if(val%coins[0]==0){
+                return val/coins[0];
+            }
+            else{
+                return 0;
+            }
+        }
+        if(dp[idx][val]!=-1){
+            return dp[idx][val];
+        }
+        int ntake=0+f(idx-1,val,coins,dp);
+        int take=1e9;
+        if(val>=coins[idx]){
+            take=1+f(idx,val-coins[idx],coins,dp);
+        }
+        return dp[idx][val]=min(take,ntake);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        if(amount==0){
+            return 0;
+        }
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans= f(n-1,amount,coins,dp);
+        if(ans==0){
+            return -1;
+        }
+        return ans;
+        
+    }
+};
