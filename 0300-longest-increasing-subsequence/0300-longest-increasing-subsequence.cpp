88f458eedb053bda7b81525b4becd3c6bff61c8c
BimalSinha59 <bimalsinha6264@gmail.com>
2025-05-20 09:29:43 +0530

    Time: 263 ms (33.95%), Space: 296.3 MB (25.57%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -19,20 +19,34 @@ public:
         // vector<vector<int>>dp(n,vector<int>(n+1,-1));
         // return f(0,-1,nums,n,dp);
 
-        vector<int>temp;
-        int len=1;
-        temp.push_back(nums[0]);
-        for(int i=1; i<n; i++){
-            if(nums[i]>temp.back()){
-                temp.push_back(nums[i]);
-                len++;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int ind=n-1;ind>=0; ind--){
+            for(int p_end=ind-1;p_end>=-1; p_end--){
+                int len=0+dp[ind+1][p_end+1];
+                if(p_end==-1 || nums[ind]>nums[p_end]){
+                    len=max(len,1+dp[ind+1][ind+1]);
+                }
+                dp[ind][p_end+1]=len;
             }
-            else{
-                int idx=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
-                temp[idx]=nums[i];
-            }
-            
         }
-        return len;
+        return dp[0][0];
+
+
+
+        // vector<int>temp;
+        // int len=1;
+        // temp.push_back(nums[0]);
+        // for(int i=1; i<n; i++){
+        //     if(nums[i]>temp.back()){
+        //         temp.push_back(nums[i]);
+        //         len++;
+        //     }
+        //     else{
+        //         int idx=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+        //         temp[idx]=nums[i];
+        //     }
+            
+        // }
+        // return len;
     }
 };
