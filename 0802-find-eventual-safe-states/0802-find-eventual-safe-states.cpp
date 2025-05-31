2c510873f49b6e563bd6b93fcd9fc09a33e3e021
BimalSinha59 <bimalsinha6264@gmail.com>
2025-05-31 10:19:12 +0530

    Time: 3 ms (95.22%), Space: 51.9 MB (83.99%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    bool dfsCheck(int node,vector<vector<int>>& graph,vector<int>& vis,vector<int>& pvis){
+    bool dfsCheck(int node,vector<vector<int>>& graph,vector<int>& vis){
         vis[node]=1;
-        pvis[node]=1;
         for(auto it:graph[node]){
             if(vis[it]==0){
-                if(dfsCheck(it,graph,vis,pvis)){
+                if(dfsCheck(it,graph,vis)){
                     return true;
                 }
             }
-            else if(vis[it]==pvis[it]){
+            else if(vis[it]==1){
                 return true;
             }
         }
-        pvis[node]=0;
+        vis[node]=2;
         return false;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<int>vis(n,0),pvis(n,0);
+        vector<int>vis(n,0);
         for(int i=0; i<n; i++){
             if(vis[i]==0){
-                dfsCheck(i,graph,vis,pvis);
+                dfsCheck(i,graph,vis);
             }
         }
         vector<int>ans;
         for(int i=0; i<n; i++){
-            if(pvis[i]==0){
+            if(vis[i]==2){
                 ans.push_back(i);
             }
         }
