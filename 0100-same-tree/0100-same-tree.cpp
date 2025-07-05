ca8181b2101a24ddfae5b8aa796ca890fd13dd69
BimalSinha59 <bimalsinha6264@gmail.com>
2025-07-06 00:24:50 +0530

    Time: 0 ms (100%), Space: 12.7 MB (78.04%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -18,10 +18,9 @@ public:
         if(p==NULL && q!=NULL || p!=NULL && q==NULL){
             return false;
         }
-        if(p->val!=q->val){
-            return false;
+        if(p->val==q->val){
+            return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
         }
-        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
-        return true;
+        return false;
     }
 };
