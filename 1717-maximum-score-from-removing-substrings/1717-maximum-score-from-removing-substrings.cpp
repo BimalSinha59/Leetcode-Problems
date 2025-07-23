22af455d020599445793cc134042b869ce2c3f68
BimalSinha59 <bimalsinha6264@gmail.com>
2025-07-23 12:07:35 +0530

    Time: 17 ms (95.08%), Space: 19.7 MB (99.36%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -2,50 +2,78 @@ class Solution {
 public:
     int maximumGain(string s, int x, int y) {
         int n = s.size();
-        vector<char> v;
-        int ans = 0;
-        if (x >= y) {
-            for (int i = 0; i < n; i++) {
-                if (s[i] == 'b' && !v.empty() && v.back() == 'a') {
-                    ans += x;
-                    v.pop_back();
-                }
-                else {
-                    v.push_back(s[i]);
-                }
+        // vector<char> v;
+        // int ans = 0;
+        // if (x >= y) {
+        //     for (int i = 0; i < n; i++) {
+        //         if (s[i] == 'b' && !v.empty() && v.back() == 'a') {
+        //             ans += x;
+        //             v.pop_back();
+        //         }
+        //         else {
+        //             v.push_back(s[i]);
+        //         }
+        //     }
+        //     vector<char>vv;
+        //     for (int i = 0; i < v.size(); i++) {
+        //         if(v[i]=='a' && !vv.empty() && vv.back()=='b'){
+        //             ans+=y;
+        //             vv.pop_back();
+        //         }
+        //         else{
+        //             vv.push_back(v[i]);
+        //         }
+        //     }
+        // } 
+        // else {
+        //     for (int i = 0; i < n; i++) {
+        //         if (s[i] == 'a' && !v.empty() && v.back() == 'b') {
+        //             ans += y;
+        //             v.pop_back();
+        //         } 
+        //         else {
+        //             v.push_back(s[i]);
+        //         }
+        //     }
+        //     vector<char>vv;
+        //     for (int i = 0; i < v.size(); i++) {
+        //         if(v[i]=='b' && !vv.empty() && vv.back()=='a'){
+        //             ans+=x;
+        //             vv.pop_back();
+        //         }
+        //         else{
+        //             vv.push_back(v[i]);
+        //         }
+        //     }
+        // }  ====>>> TC=O(n) && SC=O(n)
+
+        //////Optimization//////
+        if(x<y){
+            swap(x,y);
+            reverse(s.begin(),s.end());
+        }
+        int ans=0;
+        int acnt=0,bcnt=0;
+        for(int i=0; i<n; i++){
+            if(s[i]=='a'){
+                acnt++;
             }
-            vector<char>vv;
-            for (int i = 0; i < v.size(); i++) {
-                if(v[i]=='a' && !vv.empty() && vv.back()=='b'){
-                    ans+=y;
-                    vv.pop_back();
-                }
-                else{
-                    vv.push_back(v[i]);
-                }
-            }
-        } 
-        else {
-            for (int i = 0; i < n; i++) {
-                if (s[i] == 'a' && !v.empty() && v.back() == 'b') {
-                    ans += y;
-                    v.pop_back();
-                } 
-                else {
-                    v.push_back(s[i]);
-                }
-            }
-            vector<char>vv;
-            for (int i = 0; i < v.size(); i++) {
-                if(v[i]=='b' && !vv.empty() && vv.back()=='a'){
+            else if(s[i]=='b'){
+                if(acnt>0){
                     ans+=x;
-                    vv.pop_back();
+                    acnt--;
                 }
                 else{
-                    vv.push_back(v[i]);
+                    bcnt++;
                 }
+            }
+            else{
+                ans+=min(acnt,bcnt)*y;
+                acnt=0;
+                bcnt=0;
             }
         }
-        return ans;
+        ans+=min(acnt,bcnt)*y;
+        return ans; //====>>>TC=O(n) , SC=O(1)
     }
 };
