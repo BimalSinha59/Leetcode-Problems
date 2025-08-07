39735fc8b48741a8b20d6640b159ee4bcc2c5bd1
BimalSinha59 <bimalsinha6264@gmail.com>
2025-08-08 01:00:41 +0530

    Time: 1 ms (78.41%), Space: 10.9 MB (39.98%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -4,6 +4,26 @@ public:
         int n=s.size();
         vector<int> hash(26,0);
         int l=0,maxi=0,ans=0;
+        // for(int r=0; r<n; r++){
+        //     int idx=s[r]-'A';
+        //     hash[idx]++;
+        //     maxi=max(maxi,hash[idx]);
+        //     int len=r-l+1;
+        //     if(len-maxi<=k){
+        //         ans=max(ans,len);
+        //     }
+        //     else{
+        //         while(l<n && len-maxi>k){
+        //             hash[s[l]-'A']--;
+        //             for(int i=0; i<26; i++){
+        //                 maxi=max(maxi,hash[i]);
+        //             }
+        //             l++;
+        //             len=r-l+1;
+        //         }
+        //     }
+        // } // TC => O(2*n*26)
+
         for(int r=0; r<n; r++){
             int idx=s[r]-'A';
             hash[idx]++;
@@ -13,16 +33,12 @@ public:
                 ans=max(ans,len);
             }
             else{
-                while(l<n && len-maxi>k){
+                if(l<n && len-maxi>k){
                     hash[s[l]-'A']--;
-                    for(int i=0; i<26; i++){
-                        maxi=max(maxi,hash[i]);
-                    }
                     l++;
-                    len=r-l+1;
                 }
             }
-        }
+        } // TC => O(n)
         return ans;
     }
 };
