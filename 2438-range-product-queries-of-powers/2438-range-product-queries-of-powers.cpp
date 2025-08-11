771ebe97a61b1c0230fff5031d7d8281c0804287
BimalSinha59 <bimalsinha6264@gmail.com>
2025-08-11 14:06:54 +0530

    Time: 87 ms (26.6%), Space: 165.7 MB (5.85%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
@@ -9,14 +9,30 @@ public:
         }
         int mod = 1e9 + 7;
         vector<int> ans;
-        for (auto it : queries) {
-            int l = it[0];
-            int r = it[1];
-            int res = 1;
-            for (int i = l; i <= r; i++) {
-                res = (res * 1LL * v[i]) % mod;
+        // for (auto it : queries) {
+        //     int l = it[0];
+        //     int r = it[1];
+        //     int res = 1;
+        //     for (int i = l; i <= r; i++) {
+        //         res = (res * 1LL * v[i]) % mod;
+        //     }
+        //     ans.push_back(res);
+        // }
+
+        //precomputation
+        int m=v.size();
+        vector<vector<int>> qans(m,vector<int>(m,0));
+        for(int i=0; i<m; i++){
+            int curr=1;
+            for(int j=i; j<m; j++){
+               curr=(curr*1LL*v[j])%mod;
+               qans[i][j]=curr;
             }
-            ans.push_back(res);
+        }
+        for(auto it:queries){
+            int l=it[0];
+            int r=it[1];
+            ans.push_back(qans[l][r]);
         }
         return ans;
     }
