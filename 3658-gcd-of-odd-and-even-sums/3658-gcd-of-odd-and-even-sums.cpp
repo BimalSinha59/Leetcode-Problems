27b780db4e9458efcd281cfe78c1584c5e8dc661
BimalSinha59 <bimalsinha6264@gmail.com>
2025-08-24 11:53:31 +0530

    Time: 0 ms (100%), Space: 8.5 MB (100%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        // int sumOdd=(n*(1+2*n-1))/2;
+        // int sumEven=(n*(2+2*n))/2;
+        // return __gcd(sumOdd,sumEven);
+        return n;
+    }
+};
