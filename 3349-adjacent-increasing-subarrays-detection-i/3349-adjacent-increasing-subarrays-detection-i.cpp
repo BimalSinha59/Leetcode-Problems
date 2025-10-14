118ebf584e724bd5c3a4f6811fc9a21bd07bdea3
BimalSinha59 <bimalsinha6264@gmail.com>
2025-10-14 19:57:34 +0530

    Time: 20 ms (17.26%), Space: 40.5 MB (29.65%) - LeetHub

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.cpp
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-
-    bool isSort(vector<int>&nums,int s,int k){
-        for(int i=s; i<s+k-1; i++){
-            if(nums[i]>=nums[i+1]){
+    bool isInc(int idx, int k, vector<int>& nums) {
+        int n = nums.size();
+        if (n - idx < k) {
+            return false;
+        }
+        for (int i = idx + 1; i < idx + k && i < n; i++) {
+            if (nums[i - 1] >= nums[i]) {
                 return false;
             }
         }
         return true;
     }
-    
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
-        int n=nums.size();
-        if(n<2*k) return false;
-        for(int i=0; i<=n-2*k; i++){
-            vector<pair<int,int>>v;
-            if(isSort(nums,i,k) && (isSort(nums,i+k,k))){
+        int n = nums.size();
+        for (int i = 0; i < n - k; i++) {
+            if (isInc(i, k, nums) && isInc(i + k, k, nums)) {
                 return true;
             }
         }
