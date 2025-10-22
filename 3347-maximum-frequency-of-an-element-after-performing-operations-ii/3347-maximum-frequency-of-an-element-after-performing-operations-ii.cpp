d79c56def7228f6d7beb2e8f3907052b8f8865a6
BimalSinha59 <bimalsinha6264@gmail.com>
2025-10-22 12:37:28 +0530

    Time: 2684 ms (5.22%), Space: 419 MB (5.69%) - LeetHub

---
+++ 3347-maximum-frequency-of-an-element-after-performing-operations-ii/3347-maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        int n = nums.size();
+        map<int, int> mp, freq;
+        set<int> st;
+        for (int num : nums) {
+            freq[num]++;
+            mp[num - k]++;
+            mp[num + k + 1]--;
+            st.insert(num);
+            st.insert(num - k);
+            st.insert(num + k + 1);
+        }
+        int ans = 1;
+        int sum = 0;
+        for (int ele : st) {
+            sum += mp[ele];
+            int ext = min(sum - freq[ele], numOperations);
+            ans = max(ans, freq[ele] + ext);
+        }
+        return ans;
+    }
+};
