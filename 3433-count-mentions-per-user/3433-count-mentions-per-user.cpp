ae51f772c1f3d6cd6ee719843a603d0ddeb9ba2e
BimalSinha59 <bimalsinha6264@gmail.com>
2025-12-12 11:36:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3433-count-mentions-per-user/3433-count-mentions-per-user.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> countMentions(int numberOfUsers, vector<vector<string>>& events) {
+        vector<int>ans(numberOfUsers,0);
+        vector<pair<int,int>>v(numberOfUsers);
+        for(int i=0; i<numberOfUsers; i++){
+            v[i].first=0;
+            v[i].second=0;
+        }
+        for(auto &it:events){
+            if(it[0]=="MESSAGE"){
+                if(it[2]=="ALL"){
+                    for(int &id:ans){
+                        id++;
+                    }
+                }
+                else if(it[2]=="HERE"){
+                    for(int i=0; i<numberOfUsers; i++){
+                        if(v[i].first==0){
+                            ans[i]++;
+                        }
+                        else if(stoi(it[1])>=v[i].second+60){
+                            ans[i]++;
+                            v[i].first=0;
+                        }
+                    }
+                }
+                else{
+                    string str;
+                    for(int i=0; i<it[2].size(); i++){
+                        if(it[2][i]>='0' && it[2][i]<='9'){
+                            str.push_back(it[2][i]);
+                        }
+                        if(it[2][i]==' ' || i==it[2].size()-1){
+                            int num=stoi(str);
+                            ans[num]++;
+                            str.clear();
+                        }
+                    }
+                }
+            }
+            else{
+                int num=stoi(it[2]);
+                v[num].first=1;
+                v[num].second=stoi(it[1]);
+            }
+        }
+        return ans;
+    }
+};
