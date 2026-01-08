447ff8632e158bfd6dc8091fbcd0999bfcc96898
BimalSinha59 <bimalsinha6264@gmail.com>
2026-01-08 15:50:27 +0530

    Time: 15 ms (76%), Space: 17.6 MB (65.2%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int solve(int i, int j, vector<int>& nums1, vector<int>& nums2, int n,
               int m, vector<vector<int>>& dp) {
-                
+
         if (i == n || j == m) {
             return -1e9;
         }
@@ -19,7 +19,19 @@ public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MIN));
-        return solve(0, 0, nums1, nums2, n, m, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1e9));
+        //return solve(0, 0, nums1, nums2, n, m, dp);
+
+        //bottom-up
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                int pdt1 = nums1[i] * nums2[j];
+                int pdt2 = nums1[i] * nums2[j] + dp[i+1][j+1];
+                int pdt3 = dp[i][j+1];
+                int pdt4 = dp[i+1][j];
+                dp[i][j] = max({pdt1, pdt2, pdt3, pdt4});
+            }
+        }
+        return dp[0][0];
     }
 };
