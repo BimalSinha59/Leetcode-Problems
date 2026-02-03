4c2a40910eb20365c40ffcc867e115cb4da5d5f9
BimalSinha59 <bimalsinha6264@gmail.com>
2026-02-03 21:52:15 +0530

    Time: 0 ms (100%), Space: 14.1 MB (88.18%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int f(int ind,int p_end,vector<int>& nums, int n,vector<vector<int>> &dp){
-        if(ind==n){
+    int f(int ind, int p_end, vector<int>& nums, int n,
+          vector<vector<int>>& dp) {
+        if (ind == n) {
             return 0;
         }
-        if(dp[ind][p_end+1]!=-1){
-            return dp[ind][p_end+1];
+        if (dp[ind][p_end + 1] != -1) {
+            return dp[ind][p_end + 1];
         }
-        int len=1;
-        len = 0+f(ind+1,p_end,nums,n,dp);
-        if(p_end==-1 || nums[ind]>nums[p_end]){
-            len=max(len,1+f(ind+1,ind,nums,n,dp));
+        int len = 1;
+        len = 0 + f(ind + 1, p_end, nums, n, dp);
+        if (p_end == -1 || nums[ind] > nums[p_end]) {
+            len = max(len, 1 + f(ind + 1, ind, nums, n, dp));
         }
-        return  dp[ind][p_end+1]=len;
+        return dp[ind][p_end + 1] = len;
     }
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
         // vector<vector<int>>dp(n,vector<int>(n+1,-1));
         // return f(0,-1,nums,n,dp);
 
@@ -34,37 +35,43 @@ public:
         //     next=curr;
         // }
         // return next[0];
- 
+
         ///////TABULATION dp[n] method/////////
-        vector<int>dp(n,1);
-        int maxi=1;
-        for(int i=1; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(nums[i]>nums[j]){
-                    dp[i]=max(dp[i],1+dp[j]);
-                }
-            }
-            maxi=max(maxi,dp[i]);
-        }
-        return maxi;
-
-
-
-
-        // vector<int>temp;
-        // int len=1;
-        // temp.push_back(nums[0]);
+        // vector<int>dp(n,1);
+        // int maxi=1;
         // for(int i=1; i<n; i++){
-        //     if(nums[i]>temp.back()){
-        //         temp.push_back(nums[i]);
-        //         len++;
+        //     for(int j=0; j<i; j++){
+        //         if(nums[i]>nums[j]){
+        //             dp[i]=max(dp[i],1+dp[j]);
+        //         }
         //     }
-        //     else{
-        //         int idx=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
-        //         temp[idx]=nums[i];
-        //     }
-            
+        //     maxi=max(maxi,dp[i]);
         // }
-        // return len;
+        // return maxi;
+
+        //////*****LIS using BINARY SEARCH*****/////////
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int lisLen = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > temp.back()) {
+                temp.push_back(nums[i]);
+                lisLen++;
+            } else {
+                int low = 0, high = temp.size() - 1;
+                int idx;
+                while (low <= high) {
+                    int mid = (low + high) / 2;
+                    if (temp[mid] < nums[i]) {
+                        low = mid + 1;
+                    } else {
+                        idx = mid;
+                        high = mid - 1;
+                    }
+                }
+                temp[idx] = nums[i];
+            }
+        }
+        return lisLen;
     }
 };
