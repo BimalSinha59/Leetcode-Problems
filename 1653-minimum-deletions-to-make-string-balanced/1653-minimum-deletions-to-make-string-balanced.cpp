18fb29be3c60e9cfb64a3a42ba18ed4f572cc677
BimalSinha59 <bimalsinha6264@gmail.com>
2026-02-07 13:06:32 +0530

    Time: 68 ms (9.76%), Space: 25.5 MB (72.34%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -9,10 +9,10 @@ public:
                 break;
             }
         }
-        int ans1 = 0;
+        int cnt1 = 0;
         for (int i = idx1 + 1; i < n; i++) {
             if (s[i] == 'a') {
-                ans1++;
+                cnt1++;
             }
         }
         for (int i = n - 1; i >= 0; i--) {
@@ -21,12 +21,33 @@ public:
                 break;
             }
         }
-        int ans2 = 0;
+        int cnt2 = 0;
         for (int i = idx2 - 1; i >= 0; i--) {
             if (s[i] == 'b') {
-                ans2++;
+                cnt2++;
             }
         }
-        return min(ans1, ans2);
+        int ans = 1e9;
+        int c1 = 0;
+        for (int i = idx1; i < n; i++) {
+            ans = min(ans, c1 + cnt1);
+            if (s[i] == 'a') {
+                cnt1--;
+            } else {
+                c1++;
+            }
+        }
+        ans = min(ans, c1);
+        int c2 = 0;
+        for (int i = idx2; i >= 0; i--) {
+            ans = min(ans, c2 + cnt2);
+            if (s[i] == 'b') {
+                cnt2--;
+            } else {
+                c2++;
+            }
+        }
+        ans = min(ans, c2);
+        return ans;
     }
 };
