e8960c010321e3899d315ac6c71c575488e2d8d3
BimalSinha59 <bimalsinha6264@gmail.com>
2026-02-08 13:18:59 +0530

    Time: 73 ms (5.1%), Space: 23.1 MB (54.19%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -6,26 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-
-    int getH(TreeNode* root){
-        if(root==NULL) return 0;
-        return max(getH(root->left),getH(root->right))+1;
+    int getH(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        return max(getH(root->left), getH(root->right)) + 1;
     }
     bool isBalanced(TreeNode* root) {
-        if(root==NULL) return true;
+        if (root == NULL)
+            return true;
 
-        int lh=getH(root->left);
-        int rh=getH(root->right);
-        int Diff=abs(lh-rh);
-        bool ans=(Diff<=1);
+        int lh = getH(root->left);
+        int rh = getH(root->right);
+        int Diff = abs(lh - rh);
+        bool ans = (Diff <= 1);
 
-        bool lAns=isBalanced(root->left);
-        bool rAns=isBalanced(root->right);
+        bool lAns = isBalanced(root->left);
+        bool rAns = isBalanced(root->right);
 
         return ans && lAns && rAns;
     }
