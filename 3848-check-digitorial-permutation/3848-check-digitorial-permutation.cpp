902ce588fbc7da6dd12826e320770f5767358800
BimalSinha59 <bimalsinha6264@gmail.com>
2026-03-03 13:34:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3848-check-digitorial-permutation/3848-check-digitorial-permutation.cpp
+++ 3848-check-digitorial-permutation/3848-check-digitorial-permutation.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int fact(int n) {
-        if (n == 0) {
-            return 1;
-        }
-        int ans = 1;
-        for (int i = 1; i <= n; i++) {
-            ans *= i;
-        }
-        return ans;
-    }
     bool isDigitorialPermutation(int n) {
         long long sum = 0;
         int num = n;
         multiset<int> st1, st2;
+        vector<int> fact(10, 0);
+        fact[0] = 1;
+        for (int i = 1; i <= 9; i++) {
+            fact[i] = i * fact[i - 1];
+        }
         while (n > 0) {
             int dig = n % 10;
-            sum = sum + 1LL * fact(dig);
+            sum = sum + 1LL * fact[dig];
             st1.insert(dig);
             n /= 10;
         }
