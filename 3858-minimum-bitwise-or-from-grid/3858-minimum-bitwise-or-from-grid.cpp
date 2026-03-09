2eafb204702138f01a8c2aba36c7b91343498978
BimalSinha59 <bimalsinha6264@gmail.com>
2026-03-09 23:18:30 +0530

    Time: 4 ms (87.89%), Space: 126.5 MB (43.38%) - LeetHub

---
+++ 3858-minimum-bitwise-or-from-grid/3858-minimum-bitwise-or-from-grid.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumOR(vector<vector<int>>& grid) {
+        int ans = 0;
+        for (int i = 20; i >= 0; --i) {
+            int b = 1 << i;
+            int mask = ans | (b - 1);
+            for (auto& r : grid) {
+                bool flg = true;
+                for (int a : r) {
+                    if ((a & mask) == a) {
+                        flg = false;
+                        break;
+                    }
+                }
+                if (flg) {
+                    ans |= b;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
