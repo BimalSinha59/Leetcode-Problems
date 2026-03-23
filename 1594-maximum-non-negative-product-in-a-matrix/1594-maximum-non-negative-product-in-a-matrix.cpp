b5eed0098ac61756b40945fb708a7642dfaf68b4
BimalSinha59 <bimalsinha6264@gmail.com>
2026-03-23 12:21:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     const int MOD = 1e9 + 7;
+
+    //---------MEMOIZATION--------//
     vector<vector<pair<long long, long long>>>dp;
     pair<long long, long long> solve(int i, int j, int n, int m, vector<vector<int>>& grid) {
         if (i == n - 1 && j == m - 1) {
@@ -25,8 +27,32 @@ public:
     int maxProductPath(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        dp=vector<vector<pair<long long, long long>>>(n, vector<pair<long long, long long>>(m, {LLONG_MIN, LLONG_MAX}));
-        auto [maxRes, minRes] = solve(0, 0, n, m, grid);
-        return maxRes < 0 ? -1 : maxRes % MOD;
+        // dp=vector<vector<pair<long long, long long>>>(n, vector<pair<long long, long long>>(m, {LLONG_MIN, LLONG_MAX}));
+        // auto [maxRes, minRes] = solve(0, 0, n, m, grid);
+        // return maxRes < 0 ? -1 : maxRes % MOD;
+
+        //------BOTTOM-UP----------//
+        vector<vector<pair<long long, long long>>>dp(n, vector<pair<long long, long long>>(m, {LLONG_MIN, LLONG_MAX}));
+        dp[0][0]={grid[0][0],grid[0][0]};
+        for(int j=1; j<m; j++){
+            dp[0][j].first=dp[0][j-1].first*grid[0][j];
+            dp[0][j].second=dp[0][j-1].second*grid[0][j];
+        }
+        for(int i=1; i<n; i++){
+            dp[i][0].first=dp[i-1][0].first*grid[i][0];
+            dp[i][0].second=dp[i-1][0].second*grid[i][0];
+        }
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                long long upMax=dp[i-1][j].first;
+                long long upMin=dp[i-1][j].second;
+                long long leftMax=dp[i][j-1].first;
+                long long leftMin=dp[i][j-1].second;
+                dp[i][j].first=max({upMax*grid[i][j],upMin*grid[i][j],leftMax*grid[i][j],leftMin*grid[i][j]});
+                dp[i][j].second=min({upMax*grid[i][j],upMin*grid[i][j],leftMax*grid[i][j],leftMin*grid[i][j]});
+            }
+        }
+        auto [maxRes, minRes]=dp[n-1][m-1];
+        return maxRes<0 ? -1 : maxRes%MOD;
     }
 };
