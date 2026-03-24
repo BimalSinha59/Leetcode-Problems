d81e1f861dcd40c346b0d874752719cf8929f5af
BimalSinha59 <bimalsinha6264@gmail.com>
2026-03-24 12:06:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.cpp
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -4,26 +4,18 @@ public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> pmul(n, vector<int>(m, 0));
         vector<vector<int>> smul(n, vector<int>(m, 0));
-        long long mul1 = 1, mul2 = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                mul1 = ((mul1 % MOD) * (grid[i][j] % MOD)) % MOD;
-                mul2 = ((mul2 % MOD) * (grid[n - 1 - i][m - 1 - j] % MOD)) % MOD;
-                pmul[i][j] = mul1;
-                smul[n - 1 - i][m - 1 - j] = mul2;
+        long long mul = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                mul = ((mul % MOD) * (grid[i][j] % MOD)) % MOD;
+                smul[i][j] = mul;
             }
         }
         vector<vector<int>> ans(n, vector<int>(m, 0));
+        long long left = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                int left = 1;
-                if (j == 0 && i != 0) {
-                    left = pmul[i - 1][m - 1];
-                } else if (j != 0) {
-                    left = pmul[i][j - 1];
-                }
                 int right = 1;
                 if (j == m - 1 && i != n - 1) {
                     right = smul[i + 1][0];
@@ -31,6 +23,7 @@ public:
                     right = smul[i][j + 1];
                 }
                 ans[i][j] = ((left % MOD) * (right % MOD)) % MOD;
+                left = ((left % MOD) * (grid[i][j] % MOD)) % MOD;
             }
         }
         return ans;
