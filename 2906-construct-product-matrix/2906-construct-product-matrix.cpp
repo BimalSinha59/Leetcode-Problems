a583b1f2a55bdb0e29d357a7077a9376cf150ee6
BimalSinha59 <bimalsinha6264@gmail.com>
2026-03-24 12:20:46 +0530

    Time: 27 ms (87.45%), Space: 132.1 MB (80.07%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.cpp
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -8,19 +8,18 @@ public:
         long long mul = 1;
         for (int i = n - 1; i >= 0; i--) {
             for (int j = m - 1; j >= 0; j--) {
-                mul = ((mul % MOD) * (grid[i][j] % MOD)) % MOD;
                 smul[i][j] = mul;
+                mul = ((mul % MOD) * (grid[i][j] % MOD)) % MOD;
             }
         }
-        vector<vector<int>> ans(n, vector<int>(m, 0));
         long long left = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                int right = j + 1 == m ? (i + 1 == n ? 1 : smul[i + 1][0]) : smul[i][j + 1];
-                ans[i][j] = ((left % MOD) * (right % MOD)) % MOD;
+                int right = smul[i][j];
+                smul[i][j] = ((left % MOD) * (right % MOD)) % MOD;
                 left = ((left % MOD) * (grid[i][j] % MOD)) % MOD;
             }
         }
-        return ans;
+        return smul;
     }
 };
