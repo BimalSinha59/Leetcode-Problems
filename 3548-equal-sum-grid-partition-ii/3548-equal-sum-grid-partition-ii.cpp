55e25d656be821f7e41c4766f6727f20dad32633
BimalSinha59 <bimalsinha6264@gmail.com>
2026-03-26 22:29:38 +0530

    Time: 858 ms (64.61%), Space: 389.3 MB (58.46%) - LeetHub

---
+++ 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+    long long total;
+    bool checkHorCuts(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        unordered_set<long long> st;
+        long long top = 0;
+
+        for (int i = 0; i <= m - 2; i++) {
+
+            for (int j = 0; j < n; j++) {
+                st.insert(grid[i][j]);
+                top += grid[i][j];
+            }
+
+            long long bottom = (total - top);
+            long long diff = top - bottom;
+
+            if (diff == 0)
+                return true;
+
+            if (diff == (long long)grid[0][0])
+                return true;
+            if (diff == (long long)grid[0][n - 1])
+                return true;
+            if (diff == (long long)grid[i][0])
+                return true;
+
+            if (i > 0 && n > 1 && st.count(diff)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                total += grid[i][j];
+            }
+        }
+
+        if (checkHorCuts(grid)) {
+            return true;
+        }
+
+        reverse(begin(grid), end(grid));
+
+        if (checkHorCuts(grid)) {
+            return true;
+        }
+
+        reverse(begin(grid), end(grid));
+        vector<vector<int>> transposeGrid(n, vector<int>(m));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transposeGrid[j][i] = grid[i][j];
+            }
+        }
+
+        if (checkHorCuts(transposeGrid)) {
+            return true;
+        }
+
+        reverse(begin(transposeGrid), end(transposeGrid));
+
+        if (checkHorCuts(transposeGrid)) {
+            return true;
+        }
+        return false;
+    }
+};
