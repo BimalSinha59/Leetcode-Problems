77644e740165540b634bfa2ec3aa06d1a2c1bff2
BimalSinha59 <bimalsinha6264@gmail.com>
2026-03-30 12:29:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     bool checkStrings(string s1, string s2) {
-        string str11, str12, str21, str22;
         int n = s1.size();
+        vector<int> mp(56, 0);
         for (int i = 0; i < n; i++) {
-            if (i & 1) {
-                str11.push_back(s1[i]);
-                str21.push_back(s2[i]);
-            } else {
-                str12.push_back(s1[i]);
-                str22.push_back(s2[i]);
+            int add = (i & 1) ? 26 : 0;
+            mp[s1[i] - 'a' + add]++;
+            mp[s2[i] - 'a' + add]--;
+        }
+        for (int i = 0; i < 56; i++) {
+            if (mp[i]) {
+                return false;
             }
         }
-        sort(str11.begin(), str11.end());
-        sort(str12.begin(), str12.end());
-        sort(str21.begin(), str21.end());
-        sort(str22.begin(), str22.end());
-        return str11 == str21 && str12 == str22;
+        return true;
     }
 };
