45f2f9c0f2a4b3a8e5ccb5211160a3c88484610d
BimalSinha59 <bimalsinha6264@gmail.com>
2026-04-15 11:18:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -3,13 +3,21 @@ public:
     int closestTarget(vector<string>& words, string target, int startIndex) {
         int n = words.size();
         int i = startIndex;
-        int dis1 = 1e9, dis2 = 1e9;
+        int j = startIndex;
+        int dis = 1e9;
         while (true) {
             if (words[i] == target) {
                 if (i <= startIndex) {
-                    dis1 = startIndex - i;
+                    dis = startIndex - i;
                 } else {
-                    dis1 = startIndex + n - i;
+                    dis = startIndex + n - i;
+                }
+                break;
+            } else if (words[j] == target) {
+                if (j >= startIndex) {
+                    dis = j - startIndex;
+                } else {
+                    dis = n - startIndex + j;
                 }
                 break;
             }
@@ -17,23 +25,11 @@ public:
             if (i == startIndex) {
                 break;
             }
-        }
-        i = startIndex;
-        while (true) {
-            if (words[i] == target) {
-                if (i >= startIndex) {
-                    dis2 = i - startIndex;
-                } else {
-                    dis2 = n - startIndex + i;
-                }
-                break;
-            }
-            i = (i + 1) % n;
-            if (i == startIndex) {
+            j = (j + 1) % n;
+            if (j == startIndex) {
                 break;
             }
         }
-        int dis = min(dis1, dis2);
         return dis == 1e9 ? -1 : dis;
     }
 };
