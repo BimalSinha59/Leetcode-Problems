ddacc9797b0b5b2376e6cab4db16f6c73d4b3ea9
BimalSinha59 <bimalsinha6264@gmail.com>
2026-05-16 00:06:07 +0530

    Time: 0 ms (100%), Space: 18.4 MB (79.93%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,31 +8,31 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class Compare {
+public:
+    bool operator()(ListNode* a, ListNode* b) { return a->val > b->val; }
+};
+
 class Solution {
 public:
-    void insert(ListNode** ans, int item) {
-        ListNode* temp = new ListNode(item);
-        temp->next = *ans;
-        *ans = temp;
-    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) {
-            return NULL;
-        }
-        vector<int> v;
-        for (auto it : lists) {
-            ListNode* temp = it;
-            while (temp != NULL) {
-                v.push_back(temp->val);
-                temp = temp->next;
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
+        for (auto list : lists) {
+            if (list != NULL) {
+                pq.push(list);
             }
         }
-        sort(v.begin(), v.end());
-        ListNode* ans = NULL;
-        int n = v.size();
-        for (int i = n - 1; i >= 0; i--) {
-            insert(&ans, v[i]);
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+        while (!pq.empty()) {
+            ListNode* smallest = pq.top();
+            pq.pop();
+            tail->next = smallest;
+            tail = tail->next;
+            if (smallest->next != NULL) {
+                pq.push(smallest->next);
+            }
         }
-        return ans;
+        return dummy->next;
     }
 };
