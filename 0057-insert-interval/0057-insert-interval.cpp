e6526f5820a5035ac275485d576eb7583dba465d
BimalSinha59 <bimalsinha6264@gmail.com>
2026-05-23 23:45:59 +0530

    Time: 0 ms (100%), Space: 21.8 MB (20.42%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals,
-                               vector<int>& newInterval) {
-                                
-        vector<vector<int>> temp;
-        int i;
-        for (i = 0; i < intervals.size(); i++) {
-            if (intervals[i][0] < newInterval[0] &&
-                intervals[i][1] <= newInterval[1]) {
-                temp.push_back({intervals[i][0], intervals[i][1]});
-            } 
-            else {
-                break;
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        if (intervals.empty()) {
+            return {newInterval};
+        }
+        int n = intervals.size();
+        int idx = -1;
+        int low = 0, high = n - 1;
+        int target = newInterval[0];
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (intervals[mid][0] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-        temp.push_back({newInterval[0], newInterval[1]});
-        for (i = i; i < intervals.size(); i++) {
-            temp.push_back({intervals[i][0], intervals[i][1]});
-        }
+        intervals.insert(intervals.begin() + low, newInterval);
         vector<vector<int>> ans;
-        ans.push_back({temp[0][0], temp[0][1]});
-        for (int i = 1; i < temp.size(); i++) {
-            int ps = ans.back()[0];
-            int pe = ans.back()[1];
-            int cs = temp[i][0];
-            int ce = temp[i][1];
-            if (pe < cs) {
-                ans.push_back({cs, ce});
-            } 
-            else {
-                pe = max(pe, ce);
-                ans.back()[1] = pe;
+        ans.push_back(intervals[0]);
+        for (int i = 1; i <= n; i++) {
+            int c_start = intervals[i][0];
+            int c_end = intervals[i][1];
+            int p_start = ans.back()[0];
+            int p_end = ans.back()[1];
+            if (c_start >= p_start && c_start <= p_end) {
+                ans.back()[1] = max(p_end, c_end);
+            } else {
+                ans.push_back(intervals[i]);
             }
         }
         return ans;
