7de317de4577912f91d0ff5de38fc9b434b67032
BimalSinha59 <bimalsinha6264@gmail.com>
2026-05-24 17:45:26 +0530

    Time: 16 ms (35.93%), Space: 20.7 MB (21.56%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int dfs(int i, int d, int n, vector<int>& arr, vector<int>& dp) {
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-        int ans = 1;
-        for (int j = i - 1; abs(i - j) <= d && j >= 0 && arr[j] < arr[i]; j--) {
-            ans = max(ans, 1 + dfs(j, d, n, arr, dp));
-        }
-        for (int j = i + 1; abs(i - j) <= d && j < n && arr[j] < arr[i]; j++) {
-            ans = max(ans, 1 + dfs(j, d, n, arr, dp));
-        }
-        return dp[i] = ans;
-    }
     int maxJumps(vector<int>& arr, int d) {
         int n = arr.size();
-        int res = 1;
-        vector<int> dp(n, -1);
+        vector<pair<int, int>> ele_idx_pair;
         for (int i = 0; i < n; i++) {
-            res = max(res, dfs(i, d, n, arr, dp));
+            ele_idx_pair.push_back({arr[i], i});
+        }
+        sort(ele_idx_pair.begin(), ele_idx_pair.end());
+        int res = 1;
+        vector<int> dp(n, 1);
+        for (auto& p : ele_idx_pair) {
+            int i = p.second;
+            int ans = 1;
+            for (int j = i - 1; abs(i - j) <= d && j >= 0 && arr[j] < arr[i];
+                 j--) {
+                ans = max(ans, 1 + dp[j]);
+            }
+            for (int j = i + 1; abs(i - j) <= d && j < n && arr[j] < arr[i];
+                 j++) {
+                ans = max(ans, 1 + dp[j]);
+            }
+            dp[i] = ans;
+            res = max(res, dp[i]);
         }
         return res;
     }
