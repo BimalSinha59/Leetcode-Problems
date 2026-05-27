f2c0b49e15dc62c27dd0f679b05b52547235a1f8
BimalSinha59 <bimalsinha6264@gmail.com>
2026-05-27 10:21:01 +0530

    Time: 156 ms (8.71%), Space: 24.2 MB (89.61%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        vector<int> lower_idx(26, -1), upper_idx(26, -1);
-        for (int i = 0; i < word.size(); i++) {
-            if (word[i] >= 'a' && word[i] <= 'z') {
-                lower_idx[word[i] - 'a'] = i;
-            } else if (word[i] >= 'A' && word[i] <= 'Z' && upper_idx[word[i] - 'A'] == -1) {
-                upper_idx[word[i] - 'A'] = i;
-            }
-        }
+        int n=word.size();
         int ans = 0;
-        for (int i = 0; i < 26; i++) {
-            if (lower_idx[i] != -1 && upper_idx[i] != -1 && lower_idx[i] < upper_idx[i]) {
+        for (char ch='a' ; ch<='z'; ch++) {
+            int lidx=-1;
+            for(int i=n-1; i>=0; i--){
+                if(word[i]==ch){
+                    lidx=i;
+                    break;
+                }
+            }
+            int fidx=-1;
+            for(int i=0; i<n; i++){
+                if(word[i]==ch-'a'+'A'){
+                    fidx=i;
+                    break;
+                }
+            }
+            if(lidx!=-1 && fidx!=-1 && lidx<fidx){
                 ans++;
             }
         }
