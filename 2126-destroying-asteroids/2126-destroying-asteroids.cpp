fe19d5e0f7573f4e97159974a0c324231445ef59
BimalSinha59 <bimalsinha6264@gmail.com>
2026-05-31 10:19:22 +0530

    Time: 21 ms (95.18%), Space: 130.5 MB (5.11%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
+    vector<int> countingSort(vector<int>& nums) {
+        int n = nums.size();
+        int maxVal = 0;
+        for (int i = 0; i < n; i++) {
+            maxVal = max(maxVal, nums[i]);
+        }
+        vector<int> count(maxVal + 1, 0);
+        for (int i = 0; i < n; i++) {
+            count[nums[i]]++;
+        }
+        for (int i = 1; i <= maxVal; i++) {
+            count[i] += count[i - 1];
+        }
+        vector<int> ans(n);
+        for (int i = n - 1; i >= 0; i--) {
+            ans[count[nums[i]] - 1] = nums[i];
+            count[nums[i]]--;
+        }
+        return ans;
+    }
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-        sort(asteroids.begin(), asteroids.end());
+        asteroids = countingSort(asteroids);
         long long sum = mass;
         for (int i = 0; i < asteroids.size(); i++) {
             if (sum < asteroids[i]) {
