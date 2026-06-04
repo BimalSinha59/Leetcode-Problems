29b8bcb78e7bfad6d25a6f124a50fd7e0ef7677e
BimalSinha59 <bimalsinha6264@gmail.com>
2026-06-04 23:27:58 +0530

    Time: 0 ms (100%), Space: 17.7 MB (28.22%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-    bool bfsCheck(int start,vector<vector<int>>& graph,vector<int>& color){
-        queue<int>q;
+    bool checkBFS(int start, vector<vector<int>>& graph, vector<int>& color) {
+        queue<int> q;
         q.push(start);
-        color[start]=0;
-        while(!q.empty()){
-            int node=q.front();
+        color[start] = 0;
+        while (!q.empty()) {
+            int node = q.front();
             q.pop();
-            for(auto it:graph[node]){
-                if(color[it]==-1){
-                    color[it]=!color[node];
-                    q.push(it);
-                }
-                else if(color[it]==color[node]){
+            for (int& adjNode : graph[node]) {
+                if (color[adjNode] == -1) {
+                    color[adjNode] = !color[node];
+                    q.push(adjNode);
+                } else if (color[adjNode] == color[node]) {
                     return false;
                 }
-            }
-        }
-        return true;
-    }
-    bool dfsCheck(int start,vector<vector<int>>& graph,vector<int>& color,int col){
-        color[start]=col;
-        for(auto it:graph[start]){
-            if(color[it]==-1){
-                if(dfsCheck(it,graph,color,!col)==false){
-                    return false;
-                }
-            }
-            else if(color[it]==col){
-                return false;
             }
         }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int>color(n,-1);
-        for(int i=0; i<n; i++){
-            if(color[i]==-1){
-                if(dfsCheck(i,graph,color,0)==false){
+        int n = graph.size();
+        vector<int> color(n, -1);
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                if (!checkBFS(i, graph, color)) {
                     return false;
                 }
             }
