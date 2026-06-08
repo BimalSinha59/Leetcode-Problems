72d6b629b590d3e67f167b3a8030dfef590cd98a
BimalSinha59 <bimalsinha6264@gmail.com>
2026-06-08 18:34:38 +0530

    Time: 83 ms (12.09%), Space: 44.2 MB (8.89%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,45 +1,34 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if(grid[0][0]==1){
-            return -1;
+        int n = grid.size();
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        queue<vector<int>> q;
+        if (grid[0][0] == 0) {
+            q.push({1, 0, 0});
         }
-        int n=grid.size();
-        if(n==1){
-            return 1;
-        }
-        queue<pair<pair<int,int>,int>>q; //No need of priority queue
-        vector<vector<int>>dist(n,vector<int>(n,1e9));
-        q.push({{0,0},0});
-        dist[0][0]=0;
-        int ans=1e9;
-        while(!q.empty()){
-            int i=q.front().first.first;
-            int j=q.front().first.second;
-            int dis=q.front().second;
+        vis[0][0] = 1;
+        while (!q.empty()) {
+            auto cell = q.front();
+            int dis = cell[0];
+            int r = cell[1];
+            int c = cell[2];
             q.pop();
-            for(int di=-1; di<=1; di++){
-                for(int dj=-1; dj<=1; dj++){
-                    if(di==0 && dj==0){
-                        continue;
-                    }
-                    int ni=i+di;
-                    int nj=j+dj;
-                    if(ni>=0 && ni<n && nj>=0 && nj<n && grid[ni][nj]==0 ){
-                        if(ni==n-1 && nj==n-1){
-                            ans=min(ans,dis+1);
-                        }
-                        if(dis+1<dist[ni][nj]){
-                            dist[ni][nj]=dis+1;
-                            q.push({{ni,nj},dis+1});
-                        }
+            if (r == n - 1 && c == n - 1 && grid[r][c] == 0) {
+                return dis;
+            }
+            for (int dr = -1; dr <= 1; dr++) {
+                for (int dc = -1; dc <= 1; dc++) {
+                    int nr = r + dr;
+                    int nc = c + dc;
+                    if (nr >= 0 && nr < n && nc >= 0 && nc < n &&
+                        grid[nr][nc] == 0 && !vis[nr][nc]) {
+                        q.push({dis + 1, nr, nc});
+                        vis[nr][nc] = 1;
                     }
                 }
             }
         }
-        if(ans==1e9){
-            return -1;
-        }
-        return ans+1;
+        return -1;
     }
 };
