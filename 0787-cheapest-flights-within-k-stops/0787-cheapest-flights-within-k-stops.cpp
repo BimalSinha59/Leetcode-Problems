6341041d0ff5f4c5ccb3dd9bb7e684d8d221482f
BimalSinha59 <bimalsinha6264@gmail.com>
2026-06-08 22:43:31 +0530

    Time: 1 ms (72.84%), Space: 17.8 MB (62.03%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<pair<int,int>>adj[n];
-        for(int i=0; i<flights.size(); i++){
-            int u=flights[i][0];
-            int v=flights[i][1];
-            int w=flights[i][2];
-            adj[u].push_back({v,w});
+        vector<pair<int, int>> adjL[n];
+        for (auto& flight : flights) {
+            int from = flight[0];
+            int to = flight[1];
+            int price = flight[2];
+            adjL[from].push_back({to, price});
         }
-        queue<pair<int,pair<int,int>>>q;
-        vector<int>dist(n,1e9);
-        q.push({0,{src,0}});
-        dist[src]=0;
-        while(!q.empty()){
-            int stops=q.front().first;
-            int node=q.front().second.first;
-            int cost=q.front().second.second;
+        queue<vector<int>> q;
+        vector<int> tprice(n, 1e9);
+        q.push({0, src, 0});
+        tprice[src] = 0;
+        while (!q.empty()) {
+            auto node = q.front();
             q.pop();
-            if(stops>k){
+            int stops = node[0];
+            int fcity = node[1];
+            int fprice = node[2];
+            if (stops > k) {
                 continue;
             }
-            for(auto it:adj[node]){
-                int adjNode=it.first;
-                int edW=it.second;
-                if(edW+cost<dist[adjNode] && stops<=k){
-                    dist[adjNode]=edW+cost;
-                    q.push({stops+1,{adjNode,edW+cost}});
+            for (auto& adjCity : adjL[fcity]) {
+                int dcity = adjCity.first;
+                int dprice = adjCity.second;
+                if (fprice + dprice < tprice[dcity] && stops <= k) {
+                    tprice[dcity] = fprice + dprice;
+                    q.push({stops + 1, dcity, tprice[dcity]});
                 }
             }
-
         }
-        if(dist[dst]==1e9){
-            return -1;
-        }
-        return dist[dst];
+        return tprice[dst] == 1e9 ? -1 : tprice[dst];
     }
 };
