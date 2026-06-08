5ca52202e2fdf6eb1e33db1136946cb89069b80e
BimalSinha59 <bimalsinha6264@gmail.com>
2026-06-08 18:46:30 +0530

    Time: 77 ms (13.21%), Space: 44.3 MB (8.53%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<int>> vis(n, vector<int>(n, 0));
+        vector<vector<int>> dis(n, vector<int>(n, 1e9));
         queue<vector<int>> q;
         if (grid[0][0] == 0) {
-            q.push({1, 0, 0});
+            q.push({0, 0, 0});
         }
-        vis[0][0] = 1;
+        dis[0][0] = 0;
         while (!q.empty()) {
             auto cell = q.front();
-            int dis = cell[0];
+            int dist = cell[0];
             int r = cell[1];
             int c = cell[2];
             q.pop();
             if (r == n - 1 && c == n - 1 && grid[r][c] == 0) {
-                return dis;
+                return dist + 1;
             }
             for (int dr = -1; dr <= 1; dr++) {
                 for (int dc = -1; dc <= 1; dc++) {
                     int nr = r + dr;
                     int nc = c + dc;
                     if (nr >= 0 && nr < n && nc >= 0 && nc < n &&
-                        grid[nr][nc] == 0 && !vis[nr][nc]) {
-                        q.push({dis + 1, nr, nc});
-                        vis[nr][nc] = 1;
+                        grid[nr][nc] == 0) {
+                        if (dist + 1 < dis[nr][nc]) {
+                            q.push({dist + 1, nr, nc});
+                            dis[nr][nc] = dist + 1;
+                        }
                     }
                 }
             }
