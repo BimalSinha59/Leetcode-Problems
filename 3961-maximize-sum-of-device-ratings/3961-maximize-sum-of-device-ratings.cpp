ef47fa6d4950a5fbdf976eb056be17d031cb61fe
BimalSinha59 <bimalsinha6264@gmail.com>
2026-06-15 11:32:07 +0530

    Time: 1 ms (100%), Space: 156.6 MB (100%) - LeetHub

--- 3961-maximize-sum-of-device-ratings/3961-maximize-sum-of-device-ratings.cpp
+++ 3961-maximize-sum-of-device-ratings/3961-maximize-sum-of-device-ratings.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    pair<int, int> find_min_elements(vector<int>& nums, int& m) {
-        if (m == 1) {
+    pair<int, int> find_min_elements(vector<int>& nums, int& n) {
+        if (n == 1) {
             return {nums[0], nums[0]};
         }
         int first_mini = 1e9;
         int second_mini = 1e9;
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < n; i++) {
             if (nums[i] < first_mini) {
                 second_mini = first_mini;
                 first_mini = nums[i];
-            } else if (nums[i] < second_mini && nums[i] >= first_mini) {
+            } else if (nums[i] < second_mini) {
                 second_mini = nums[i];
             }
         }
         return {first_mini, second_mini};
     }
     long long maxRatings(vector<vector<int>>& units) {
-        int n = units.size();
-        int m = units[0].size();
+        int n = units[0].size();
         long long sum = 0;
         int smallest = 1e9;
         int min_second = 1e9;
         for (auto& unit : units) {
-            auto it = find_min_elements(unit, m);
+            auto it = find_min_elements(unit, n);
             sum += it.second;
             smallest = min(smallest, it.first);
             min_second = min(min_second, it.second);
         }
-        if (m == 1) {
+        if (n == 1) {
             return sum;
         }
         long long rating_sum = sum + smallest - min_second;
