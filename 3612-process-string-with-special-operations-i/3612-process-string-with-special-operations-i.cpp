bfdd1f7a1bcc9fe6537bc79cdb49da80a07bc0fd
BimalSinha59 <bimalsinha6264@gmail.com>
2026-06-16 11:33:38 +0530

    Time: 11 ms (15.15%), Space: 39.8 MB (42.05%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     string processStr(string s) {
-        int n=s.size();
-        string ans="";
-        for(int i=0; i<n; i++){
-            if(s[i]>='a' && s[i]<='z'){
-                ans+=s[i];
-            }
-            else if(s[i]=='*' && !ans.empty()){
-                ans.pop_back();
-            }
-            else if(s[i]=='#'){
-                ans+=ans;
-            }
-            else{
-                reverse(ans.begin(),ans.end());
+        string result;
+        for (char& ch : s) {
+            if (ch >= 'a' && ch <= 'z') {
+                result.push_back(ch);
+            } else if (ch == '*') {
+                if (!result.empty()) {
+                    result.pop_back();
+                }
+            } else if (ch == '#') {
+                result += result;
+            } else {
+                reverse(result.begin(), result.end());
             }
         }
-        return ans;
+        return result;
     }
 };
