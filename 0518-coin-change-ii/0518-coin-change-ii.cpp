5d074e310db09679a0b24f9351afc9584e7f50d4
BimalSinha59 <bimalsinha6264@gmail.com>
2026-06-18 18:22:55 +0530

    Time: 29 ms (53.07%), Space: 36.7 MB (38.54%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int f(int ind,int amt,vector<int>& coins,vector<vector<int>>& dp){
-        if(ind==0){
-            if(amt%coins[0]==0){
-                return 1;
-            }
-            else{
-                return 0;
-            }
+    int no_of_combinations(int idx, int amount, vector<int>& coins, vector<vector<int>>& dp) {
+        if (idx == 0) {
+            return amount % coins[0] == 0;
         }
-        if(dp[ind][amt]!=-1){
-            return dp[ind][amt];
+        if (dp[idx][amount] != -1) {
+            return dp[idx][amount];
         }
-        int ntake=f(ind-1,amt,coins,dp);
-        int take=0;
-        if(amt>=coins[ind]){
-            take=f(ind,amt-coins[ind],coins,dp);
+        int not_take = no_of_combinations(idx - 1, amount, coins, dp);
+        int take = 0;
+        if (amount >= coins[idx]) {
+            take = no_of_combinations(idx, amount - coins[idx], coins, dp);
         }
-        return dp[ind][amt]=take+ntake;
+        return dp[idx][amount] = take + not_take;
     }
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return f(n-1,amount,coins,dp);
-        
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        return no_of_combinations(n - 1, amount, coins, dp);
     }
 };
