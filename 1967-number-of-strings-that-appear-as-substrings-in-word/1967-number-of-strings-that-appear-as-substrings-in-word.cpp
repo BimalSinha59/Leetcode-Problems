a846f35a620d4c0edcedc37cb7ff35ed1bc94124
BimalSinha59 <bimalsinha6264@gmail.com>
2026-06-29 13:38:23 +0530

    Time: 8 ms (9.95%), Space: 13.3 MB (9.71%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,9 +1,52 @@
 class Solution {
 public:
+    vector<int> computeLPS(string& pattern) {
+        int n = pattern.size();
+        vector<int> lps(n, 0);
+        int len = 0;
+        int i = 1;
+        while (i < n) {
+            if (pattern[i] == pattern[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+
+    bool KMP(string& word, string& pattern) {
+        vector<int> lps = computeLPS(pattern);
+        int i = 0;
+        int j = 0;
+        while (i < word.size()) {
+            if (word[i] == pattern[j]) {
+                i++;
+                j++;
+            }
+            if (j == pattern.size()) {
+                return true;
+            } else if (i < word.size() && word[i] != pattern[j]) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
     int numOfStrings(vector<string>& patterns, string word) {
         int ans = 0;
         for (string& pattern : patterns) {
-            if (word.find(pattern) != string::npos) {
+            if (KMP(word, pattern)) {
                 ans++;
             }
         }
