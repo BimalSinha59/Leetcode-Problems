1dcd0e1bca15411981fbf56e53958a85baba406c
BimalSinha59 <bimalsinha6264@gmail.com>
2026-07-07 23:55:01 +0530

    Time: 284 ms (49.16%), Space: 10.6 MB (65.82%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    int di[4] = {-1, 0, 1, 0};
-    int dj[4] = {0, 1, 0, -1};
-    bool solve(int i, int j, int idx, int& n, int& m, int sz, vector<vector<char>>& board, string& word) {
-        if (idx == sz) {
+    vector<int> di = {0, 1, 0, -1};
+    vector<int> dj = {1, 0, -1, 0};
+    bool isValid(int i, int j, int& m, int& n) {
+        return i >= 0 && i < m && j >= 0 && j < n;
+    }
+    bool canBeConstructed(int i, int j, int& m, int& n, vector<vector<char>>& board, int idx, int& len, string& word) {
+        if (idx == len) {
             return true;
         }
-        if (i < 0 || j < 0 || i >= n || j >= m || board[i][j] != word[idx] ||
-            board[i][j] == '#') {
+        if (!isValid(i, j, m, n) || board[i][j] != word[idx]) {
             return false;
         }
-        char ch = board[i][j];
-        board[i][j] = '#';
+        bool ans = false;
+        char temp = board[i][j];
+        board[i][j] = '%';
         for (int k = 0; k < 4; k++) {
             int ni = i + di[k];
             int nj = j + dj[k];
-            if (solve(ni, nj, idx + 1, n, m, sz, board, word)) {
-                return true;
-            }
+            ans |= canBeConstructed(ni, nj, m, n, board, idx + 1, len, word);
         }
-        board[i][j] = ch;
-        return false;
+        board[i][j] = temp;
+        return ans;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == word[0] && solve(i, j, 0, n, m, word.size(), board, word)) {
+        int m = board.size();
+        int n = board[0].size();
+        int len = word.size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (canBeConstructed(i, j, m, n, board, 0, len, word)) {
                     return true;
                 }
             }
