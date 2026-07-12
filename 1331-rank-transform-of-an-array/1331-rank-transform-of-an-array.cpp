7abb44b14faf8d01efefcbe9a889108342f68108
BimalSinha59 <bimalsinha6264@gmail.com>
2026-07-12 09:33:16 +0530

    Time: 15 ms (99.47%), Space: 39.8 MB (83.39%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         int n = arr.size();
-        map<int, vector<int>> mp;
+        vector<pair<int, int>> ele_idx;
         for (int i = 0; i < n; i++) {
-            mp[arr[i]].push_back(i);
+            ele_idx.push_back({arr[i], i});
         }
-        vector<int> ans(n);
-        int rank = 1;
-        for (auto& it : mp) {
-            for (int& idx : it.second) {
-                ans[idx] = rank;
+        sort(ele_idx.begin(), ele_idx.end());
+        vector<int> ranks(n);
+        int i = 0;
+        int rank = 0;
+        while (i < n) {
+            if (i - 1 >= 0 && ele_idx[i].first == ele_idx[i - 1].first) {
+                ranks[ele_idx[i].second] = rank;
+            } else {
+                rank++;
+                ranks[ele_idx[i].second] = rank;
             }
-            rank++;
+            i++;
         }
-        return ans;
+        return ranks;
     }
 };
