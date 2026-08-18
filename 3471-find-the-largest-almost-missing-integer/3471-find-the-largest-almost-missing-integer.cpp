7057523a2b3e0c6db7bcb514c0db37bf452143ed
BimalSinha59 <bimalsinha6264@gmail.com>
2026-08-18 20:15:28 +0530

    Time: 0 ms (100%), Space: 29.4 MB (51.5%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
-        int n=nums.size();
-        int ans=-1;
-        vector<vector<int>>v;
-        for(int i=0; i<n; i++){
-            vector<int>v1;
-            for(int j=i; j<n; j++){
-                v1.push_back(nums[j]);
-                if(v1.size()==k){
-                    v.push_back(v1);
-                    break;
-                }
-               
-            }
-    
+        map<int, int> freq;
+        for (int& num : nums) {
+            freq[num]++;
         }
-        for(int i=0; i<n; i++){
-            int c=0;
-            for(int j=0; j<v.size();j++){
-                
-                for(int l=0; l<v[j].size(); l++){
-                    if(nums[i]==v[j][l]){
-                        c++;
-                        break;
-                    }
-                }
-                
-            }
-            if(c==1){
-                //cout<<nums[i]<<" ";
-                    ans=max(ans,nums[i]);
-                }
+        if (k == nums.size()) {
+            return freq.rbegin()->first;
         }
-        return ans;
-        
+        for (auto it = freq.rbegin(); it != freq.rend(); it++) {
+            if (it->second == 1 && k == 1) {
+                return it->first;
+            }
+            if (it->second == 1 &&
+                (it->first == nums[0] || it->first == nums[nums.size() - 1])) {
+                return it->first;
+            }
+        }
+        return -1;
     }
 };
