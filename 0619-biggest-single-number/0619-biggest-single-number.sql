59f5f2a8aafd0b85646149f4efb6854e32f67df9
BimalSinha59 <bimalsinha6264@gmail.com>
2026-08-22 16:55:49 +0530

    Time: 444 ms (69.04%), Space: 0B (100%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+WITH SingleNums AS(
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+)
+SELECT MAX(num) AS num
+FROM SingleNums
