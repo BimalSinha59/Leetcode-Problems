c5360b568ebfb679ae03dfaaad52eddaae2546b4
BimalSinha59 <bimalsinha6264@gmail.com>
2026-09-14 16:53:10 +0530

    Time: 7 ms (90.98%), Space: 35 MB (54.36%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int pfunc(vector<int>& weights, int n, int cap) {
+    int pred_func(vector<int>& weights, int n, int cap) {
         int sum = 0;
         int days = 0;
         for (int i = 0; i < n; i++) {
@@ -15,16 +15,16 @@ public:
     }
     int shipWithinDays(vector<int>& weights, int days) {
         int n = weights.size();
-        int s = *max_element(weights.begin(), weights.end());
-        int e = accumulate(weights.begin(), weights.end(), 0);
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
         int ans;
-        while (s <= e) {
-            int mid = (s + e) / 2;
-            if (pfunc(weights, n, mid) <= days) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (pred_func(weights, n, mid) <= days) {
                 ans = mid;
-                e = mid - 1;
+                high = mid - 1;
             } else {
-                s = mid + 1;
+                low = mid + 1;
             }
         }
         return ans;
