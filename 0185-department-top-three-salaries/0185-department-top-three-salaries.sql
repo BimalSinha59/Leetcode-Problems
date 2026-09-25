2ab12c56dd27a6203dff4b6104d07da8506645f2
BimalSinha59 <bimalsinha6264@gmail.com>
2026-09-26 01:22:56 +0530

    Time: 880 ms (97.13%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,17 +1,17 @@
 # Write your MySQL query statement below
 WITH Salary_Rank AS(
     SELECT 
-        d.name AS department_name,
-        e.name AS employee_name,
-        e.salary AS emp_salary,
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
         DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS dense_rank_val
     FROM Employee AS e
     LEFT JOIN Department AS d
     ON e.departmentId = d.id
 )
 SELECT 
-   department_name AS Department,
-   employee_name AS Employee,
-   emp_salary AS Salary  
+   Department,
+   Employee,
+   Salary  
 FROM Salary_Rank
 WHERE dense_rank_val <= 3;
